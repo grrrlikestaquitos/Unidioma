857e84e8a5319b555e0a2353037e404133000adf
Andrei <andreicrimson@gmail.com>
2019-07-02 17:12:59 -0700

    Implemented character frequency setting

---
+++ Hirakana WatchKit Extension/Settings/Frequency/CharacterFrequencyView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct CharacterFrequencyView: View {
+    @ObjectBinding var dataStore: SettingsDataStore
+
+    var body: some View {
+        List {
+            SelectionItem("Once per day", count: 1)
+            SelectionItem("Twice per day", count: 2)
+            SelectionItem("Three times per day", count: 3)
+        }
+    }
+
+    private func SelectionItem(_ text: String, count: Int) -> some View {
+        Button(action: {
+            self.dataStore.charPerDay["charPerDay"] = count
+        }) {
+            HStack {
+                HKText(textType: .subtitle, text: text)
+                Spacer()
+                if self.dataStore.charPerDay["charPerDay"] == count {
+                    Image(systemName: "checkmark.circle")
+                }
+            }
+        }
+    }
+}
+
+//#if DEBUG
+//struct CharacterFrequencyView_Previews : PreviewProvider {
+//    static var previews: some View {
+//        CharacterFrequencyView()
+//    }
+//}
+//#endif

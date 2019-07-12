e7e3f8e8bcb77313b9655ab727b54868d0f746b7
Katherine Li <katherineli0514@gmail.com>
2019-07-12 16:37:15 -0700

    * Implemented MainController to switch different views and titles based on settings. * Refactored HirakanaView to make sure we can only change view series in MainController viewType now. * Added a Constants file under Support files folder to store constants.

--- Hirakana WatchKit Extension/Main/HirakanaView.swift
+++ Hirakana WatchKit Extension/Main/HirakanaView.swift
@@ -11,14 +11,30 @@ enum ViewType {
 
 struct HirakanaView : View {
     var viewType: ViewType? = .kanji(Series.A)
-
     let kanjimodel = KanjiModel(hiragana: "わたし",
                                 kanji: "私",
                                 romaji: "wa-ta-shi",
                                 meanings: ["I; Me"],
                                 phrases: ["I fell", "私は落ちた", "Watashi wa ochita"])
-
+    let characterModel = CharacterModel(pinyin: "wǒ", character: "我", phraseCharacters: ["我","跑"], meaning: ["I; me; my"], pinyinForPhrase: ["wǒ","paǒ"], meaningForPhrase: "I run")
+    
     var body: some View {
-        KanjiViewSB(model: kanjimodel)
+        switch viewType {
+        case .kanji(let series)?:
+            if series == Series.A {
+                return AnyView(KanjiViewSA(model: kanjimodel))
+            } else {
+                return AnyView(KanjiViewSB(model: kanjimodel))
+            }
+        case .character(let series)?:
+            if series == Series.A {
+                return AnyView(CharacterViewSA(model: characterModel))
+            } else {
+                return AnyView(CharacterViewSB(model: characterModel))
+            }
+        default:
+            return AnyView(KanjiViewSA(model: kanjimodel))
+        }
     }
+    
 }

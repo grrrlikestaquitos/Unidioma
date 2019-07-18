5c1bfa6c38c05e8e91b19b8213091cb575481fe4
Andrei <andreicrimson@gmail.com>
2019-07-17 23:12:33 -0700

    removed example code

--- Hirakana WatchKit Extension/Settings/SettingsController.swift
+++ Hirakana WatchKit Extension/Settings/SettingsController.swift
@@ -5,22 +5,10 @@ import Combine
 
 class SettingsController: WKHostingController<SettingsView> {
     let settingsDataStore = SettingsDataStore()
-    let http = HTTP()
 
     override func willActivate() {
         super.willActivate()
         setTitle("Settings")
-
-        let route = CharacterRoute(route: .range(.kanji, ((from: 0, to: 5))))
-        http.request(route: route, model: [String: String].self) { response in
-            switch (response) {
-                case .success(let data):
-                    print("data: \(data)")
-                case .failure(let error):
-                    print(error)
-                break
-            }
-        }
     }
 
     override var body: SettingsView {

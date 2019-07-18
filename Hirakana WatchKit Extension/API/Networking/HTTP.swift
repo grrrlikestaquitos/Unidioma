b2beab5d7a6b7bccc294a31c7a16cdff56f3007d
Andrei <andreicrimson@gmail.com>
2019-07-17 23:05:21 -0700

    refactor typealias scope

--- Hirakana WatchKit Extension/API/Networking/HTTP.swift
+++ Hirakana WatchKit Extension/API/Networking/HTTP.swift
@@ -3,23 +3,23 @@ import Foundation
 fileprivate let session = URLSession.shared
 fileprivate let baseURL = "http://localhost:7555"
 
-typealias closure = (Response) -> ()
-
 enum Response {
     case success(Codable)
     case failure(Error)
 }
 
 struct HTTP {
+    typealias response = (Response) -> ()
+
     private let decoder = JSONDecoder()
 
-    public func request<M: Codable> (route: BaseRoute, model: M.Type, completed: @escaping closure) {
+    public func request<M: Codable> (route: BaseRoute, model: M.Type, completed: @escaping response) {
         guard let url = URL(string: baseURL + route.buildRouteUrl()) else { return }
 
         session.dataTask(with: url) { (data, responseInfo, error) in
             if let data = data {
                 do {
-                    let model: M = try self.parseJSON(data: data)
+                    let model: M = try self.serializeJSON(data: data)
                     completed(.success(model))
                 } catch (let parseError) {
                     completed(.failure(parseError))
@@ -30,10 +30,8 @@ struct HTTP {
             }
         }.resume()
     }
-}
 
-extension HTTP {
-    private func parseJSON<M: Codable>(data: Data) throws -> M {
+    private func serializeJSON<M: Codable>(data: Data) throws -> M {
         return try decoder.decode(M.self, from: data)
     }
 }
